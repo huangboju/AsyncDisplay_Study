20421cfdd345644c2e7bf812a11fa5eb2e6f0d98
huangboju <bhuang@xiaohongshu.com>
2019-01-31 21:25:25 +0800

    完成布局

--- AsyncDisplay/FeedImageController.swift
+++ AsyncDisplay/FeedImageController.swift
@@ -15,19 +15,25 @@ class FeedImageController: ASViewController<ASDisplayNode> {
         
         var nodes: [ASLayoutElement] = []
         
-        for result in [[1,2], [1,2,3]] {
+        for result in [[1], [1, 2, 3]] {
             let stack = ASStackLayoutSpec.horizontal()
             stack.spacing = 4
             nodes.append(stack)
-            var imageNodes: [ASRatioLayoutSpec] = []
+            var imageNodes: [ASNetworkImageNode] = []
             for _ in result {
                 let imageNode = ASNetworkImageNode()
                 imageNode.backgroundColor = ASDisplayNodeDefaultPlaceholderColor()
-                imageNode.style.preferredSize = CGSize(width: 80, height: 80)
-                let ratio = ASRatioLayoutSpec(ratio: result.count == 1 ? 160 / 248 : 1, child: imageNode)
-                ratio.style.flexGrow = 1
+                let preferredSize: CGSize
+                if result.count == 1 {
+                    preferredSize = CGSize(width: 248, height: 160)
+                } else if result.count == 2 {
+                    preferredSize = CGSize(width: 122, height: 122)
+                } else {
+                    preferredSize = CGSize(width: 80, height: 80)
+                }
+                imageNode.style.preferredSize = preferredSize
                 node.addSubnode(imageNode)
-                imageNodes.append(ratio)
+                imageNodes.append(imageNode)
             }
             stack.children = imageNodes
         }
@@ -36,7 +42,6 @@ class FeedImageController: ASViewController<ASDisplayNode> {
             let stack = ASStackLayoutSpec.vertical()
             stack.children = nodes
             stack.spacing = 4
-            stack.style.minSize = CGSize(width: 248, height: 248)
             
             return ASCenterLayoutSpec(centeringOptions: .XY, sizingOptions: .minimumXY, child: stack)
         }
